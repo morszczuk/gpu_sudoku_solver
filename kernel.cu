0ad7bd1be167b2259106fe90d381ab805f652cee
mmarczuk <miron.marczuk@in4mates.com>
2018-02-21 11:47:46 +0100

    fdsa

--- kernel.cu
+++ kernel.cu
@@ -14,6 +14,26 @@ __global__ void checkQuizFill(int d_quiz[SUD_SIZE][SUD_SIZE], int d_fill)
 	//] = d_quiz[idx][idy] > 0 ? 1 : 0;
 }
 
+__global__ void addNumberPresenceArray(int* d_array, int size)
+{
+	int idx = blockDim.x*blockIdx.x + threadIdx.x;
+
+	for (int i = 1; i <= size / 2; i *= 2)
+	{
+		if (idx % (2 * i) == 0) {
+			printf("BEFORE [Thread %d]: %d\n", idx, d_array[idx]);
+			d_array[idx] += d_array[idx + i];
+			printf("AFTER [Thread %d]: %d\n", idx, d_array[idx]);
+		}
+		else
+		{
+			printf("[Thread %d] returning\n", idx);
+			return;
+		}
+		__syncthreads();
+	}
+}
+
 __global__ void checkCorrectness(int* d_sudoku, int* d_number_presence)
 {
 	extern __shared__ int number_presence[];
@@ -55,6 +75,7 @@ cudaError_t solveSudoku(int* h_sudoku_quiz)
 	int *d_sudoku_quiz, *d_quiz_fill, *d_number_presence;
 	int sharedMemorySize;
 	int* h_number_presence = new int[243];
+	int* result = new int[1];
 
 	cudaErrorHandling(cudaMalloc((void **)&d_sudoku_quiz, SUD_SIZE * SUD_SIZE * sizeof(int)));
 	cudaErrorHandling(cudaMalloc((void **)&d_quiz_fill, SUD_SIZE * SUD_SIZE * sizeof(int)));
@@ -70,7 +91,7 @@ cudaError_t solveSudoku(int* h_sudoku_quiz)
 	cudaErrorHandling(cudaDeviceSynchronize());
 	cudaErrorHandling(cudaMemcpy(h_number_presence, d_number_presence, 243 * sizeof(int), cudaMemcpyDeviceToHost));
 	cudaErrorHandling(cudaDeviceSynchronize());
-	
+
 	for (int i = 0; i <27; i++)
 	{
 		for(int j = 0; j < 9; j++)
@@ -79,6 +100,18 @@ cudaError_t solveSudoku(int* h_sudoku_quiz)
 		}
 		printf("\n");
 	}
+
+	dim3 dimBlock2 = dim3(243, 1, 1);
+	dim3 dimGrid2 = dim3(1);
+	addNumberPresenceArray <<<dimGrid2, dimGrid2>>> (d_number_presence, 243);
+	cudaErrorHandling(cudaDeviceSynchronize());
+	cudaErrorHandling(cudaMemcpy(result, d_number_presence, sizeof(int), cudaMemcpyDeviceToHost));
+	cudaErrorHandling(cudaDeviceSynchronize());
+	
+	printf("---------- FINAL RESULT!!! ------\n");
+	printf("WARTOSC: %d\n", result[0]);
+
+
 	//int h_sudoku_quiz[SUD_SIZE][SUD_SIZE];
 
 	//for(int i = 0; i < SUD_SIZE; i++)
