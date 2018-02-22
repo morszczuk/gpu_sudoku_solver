bb14834c22bd106b5110f8a018fa9b2c440569ad
mmarczuk <miron.marczuk@in4mates.com>
2018-02-22 12:41:18 +0100

    work

--- kernel.cu
+++ kernel.cu
@@ -14,46 +14,46 @@ __global__ void __prescan(int *g_odata, int *g_idata, int n)
   int offset = 1;
 	printf("THID: [%d]\n", thid);
 
-  temp[2*thid] = g_idata[2*thid]; // load input into shared memory
-  temp[2*thid+1] = g_idata[2*thid+1];
+  // temp[2*thid] = g_idata[2*thid]; // load input into shared memory
+  // temp[2*thid+1] = g_idata[2*thid+1];
 
 
-  for (int d = n>>1; d > 0; d >>= 1) // build sum in place up the tree
-  {
-    __syncthreads();
-    if (thid < d)
-    {
-      int ai = offset*(2*thid+1)-1;
-      int bi = offset*(2*thid+2)-1;
-      temp[bi] += temp[ai];
-    }
-    offset *= 2;
-  }
+  // for (int d = n>>1; d > 0; d >>= 1) // build sum in place up the tree
+  // {
+  //   __syncthreads();
+  //   if (thid < d)
+  //   {
+  //     int ai = offset*(2*thid+1)-1;
+  //     int bi = offset*(2*thid+2)-1;
+  //     temp[bi] += temp[ai];
+  //   }
+  //   offset *= 2;
+  // }
 
 
-  if (thid == 0) { temp[n - 1] = 0; } // clear the last element
+  // if (thid == 0) { temp[n - 1] = 0; } // clear the last element
 
 
-  for (int d = 1; d < n; d *= 2) // traverse down tree & build scan
-  {
-    offset >>= 1;
-    __syncthreads();
-    if (thid < d)
-    {
-      int ai = offset*(2*thid+1)-1;
-      int bi = offset*(2*thid+2)-1;
-      int t = temp[ai];
-      temp[ai] = temp[bi];
-      temp[bi] += t;
-    }
-  }
+  // for (int d = 1; d < n; d *= 2) // traverse down tree & build scan
+  // {
+  //   offset >>= 1;
+  //   __syncthreads();
+  //   if (thid < d)
+  //   {
+  //     int ai = offset*(2*thid+1)-1;
+  //     int bi = offset*(2*thid+2)-1;
+  //     int t = temp[ai];
+  //     temp[ai] = temp[bi];
+  //     temp[bi] += t;
+  //   }
+  // }
 
 
-  __syncthreads();
+  // __syncthreads();
 
 
-  g_odata[2*thid] = temp[2*thid]; // write results to device memory
-  g_odata[2*thid+1] = temp[2*thid+1];
+  // g_odata[2*thid] = temp[2*thid]; // write results to device memory
+  // g_odata[2*thid+1] = temp[2*thid+1];
 } 
 
 bool defineIfSudokuIsSolved(int* d_number_presence_summed)
