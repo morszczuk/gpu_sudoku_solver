f58bb8bcab7e43a1f6170ca15798c81443d93890
mmarczuk <miron.marczuk@in4mates.com>
2018-02-22 12:34:03 +0100

    work

--- kernel.cu
+++ kernel.cu
@@ -257,11 +257,12 @@ int* scanNumberPresenceInRow(int* d_number_presence_in_row)
 	cudaErrorHandling(cudaMalloc((void **)&d_scanned_number_presence_in_row, SUD_SIZE * SUD_SIZE * sizeof(int)));
 
 	printf("\n\n\nPRZED SCANEM\n\n\n");
-	__prescan <<<dimGrid, dimBlock, sharedMemorySize>>> (d_scanned_number_presence_in_row, d_number_presence_in_row, 9);
+	// __prescan <<<dimGrid, dimBlock, sharedMemorySize>>> (d_scanned_number_presence_in_row, d_number_presence_in_row, 9);
 
-	cudaErrorHandling(cudaDeviceSynchronize());
+	// cudaErrorHandling(cudaDeviceSynchronize());
 
-	displaySudokuArray(d_scanned_number_presence_in_row);
+	// displaySudokuArray(d_scanned_number_presence_in_row);
+	return d_scanned_number_presence_in_row;
 }
 
 int* createSolution(int* d_quiz_unsolved)
