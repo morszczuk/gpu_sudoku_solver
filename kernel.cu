dc9da007a8a080682459e2790d58e3ac0c4101fa
mmarczuk <miron.marczuk@in4mates.com>
2018-02-22 12:49:39 +0100

    work

--- kernel.cu
+++ kernel.cu
@@ -16,7 +16,7 @@ __global__ void __prescan(int *g_odata, int *g_idata, int n)
 
 	printf("---\ntemp[2*%d] = %d\ntemp[2*%d] =%d\n", thid, temp[2*thid], thid, temp[2*thid+1]);
   temp[2*thid] = g_idata[2*thid]; // load input into shared memory
-  if(thid < 5)
+  if(thid < 4)
 	{
 		printf("2*thid %d\n", thid);
 		temp[2*thid+1] = g_idata[2*thid+1];
