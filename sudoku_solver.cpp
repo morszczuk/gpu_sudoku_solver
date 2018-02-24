7e8945b03f615613869fc3a7cf7ff9062018bb0f
mmarczuk <miron.marczuk@in4mates.com>
2018-02-24 16:03:20 +0100

    work

--- sudoku_solver.cpp
+++ sudoku_solver.cpp
@@ -5,16 +5,18 @@ int main(int argc, char* argv[])
 	char filename_unsolved[] = "quizzes/arr_1_unsolved.txt";
   char filename_solved[] = "quizzes/arr_1_solved.txt";
   char filename_error[] = "quizzes/arr_1_with_error.txt";
-	int *h_sudoku_solved, *h_sudoku_unsolved, *h_sudoku_error;
+	char almost_solved[] = "quizzes/arr_1_almost_solved.txt";
+	int *h_sudoku_solved, *h_sudoku_unsolved, *h_sudoku_error, *h_sudoku_almost_solved;
 	int a =5;
 	
 	//RETRIEVING SUDOKU QUIZ
 
-      h_sudoku_error = readSudokuArray(filename_error);
+	h_sudoku_error = readSudokuArray(filename_error);
     
-	    h_sudoku_unsolved = readSudokuArray(filename_unsolved);
+	h_sudoku_unsolved = readSudokuArray(filename_unsolved);
   
-    h_sudoku_solved = readSudokuArray(filename_solved);
+  h_sudoku_solved = readSudokuArray(filename_solved);
+	h_sudoku_almost_solved = readSudokuArray(almost_solved);
 	
   printArray(h_sudoku_unsolved, SUD_SIZE, SUD_SIZE);
 
@@ -22,7 +24,7 @@ int main(int argc, char* argv[])
 	clock_t begin = clock();
 	
 	//SOLVING SUDOKU 
-	cudaErrorHandling(solveSudoku(h_sudoku_solved, h_sudoku_unsolved));
+	cudaErrorHandling(solveSudoku(h_sudoku_solved, h_sudoku_almost_solved));
 
 	//ENDING TIME MEASURMENT
 	clock_t end = clock();
