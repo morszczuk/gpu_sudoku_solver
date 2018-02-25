5a2a6a6b5d9987cc8e98afe368b8770c5fb935f2
mmarczuk <miron.marczuk@in4mates.com>
2018-02-25 21:53:15 +0100

    work

--- sudoku_kernel.cu
+++ sudoku_kernel.cu
@@ -446,13 +446,11 @@ bool** checkAlternativeSolutionsCorrectness(int n_factorial, int* alternative_so
 	cudaErrorHandling(cudaMemcpy(h_alternative_solutions_correctness, d_alternative_solutions_correctness, n_factorial * sizeof(bool), cudaMemcpyDeviceToHost));
 	for(int i = 0; i < n_factorial; i++)
 	{
-		printf("\n\nRozwiazanie %d: ", i);
+		printf("\nRozwiazanie %d: ", i);
 		if(h_alternative_solutions_correctness[i])
 			printf("ZLE\n");
 		else
 			printf("OK\n");
-			
-		
 	}
 }
 
