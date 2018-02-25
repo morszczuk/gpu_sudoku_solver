40e6df10a0633c0da0d452ca3ca2e8f6f80a2d16
mmarczuk <miron.marczuk@in4mates.com>
2018-02-25 22:06:19 +0100

    work

--- sudoku_kernel.cu
+++ sudoku_kernel.cu
@@ -418,7 +418,7 @@ __global__ void __checkAlternativeSolutionsCorrectness(int* d_alternative_soluti
 
 	__syncthreads();
 
-	printf("IDX: %d | WARTOSC: %d | INDEKS DO WSTAIWENIA: %d\n", idx, d_alternative_solutions_one_array[idx], blockStart + threadIdx.x * NN + d_alternative_solutions_one_array[idx] - 1);
+	printf("IDX: %d | WARTOSC: %d | INDEKS DO WSTAIWENIA: %d\n", idx, d_alternative_solutions_one_array[idx], blockStart + (threadIdx.x * NN) + d_alternative_solutions_one_array[idx] - 1);
 	if(d_alternative_solutions_one_array[idx] > 0)
 		d_number_presence_in_row[blockStart + threadIdx.x * NN + d_alternative_solutions_one_array[idx] - 1] += 1; //informs about number data[idx][idy] - 1 presence in column idy
 	//number_presence[k + (idy * SUD_SIZE + d_sudoku[idx*SUD_SIZE + idy] - 1)] = 1; //informs about number data[idx][idy] - 1 presence in column idy
