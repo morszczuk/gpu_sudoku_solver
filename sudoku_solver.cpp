d2af8083f4c7247b1f1fdc114a6e9ce46be69a0b
mmarczuk <miron.marczuk@in4mates.com>
2018-02-28 08:38:10 +0100

    work

--- sudoku_solver.cpp
+++ sudoku_solver.cpp
@@ -2,39 +2,50 @@
 
 int main(int argc, char* argv[])
 {
-	char filename_unsolved[] = "quizzes/arr_6_unsolved.txt";
+	
   char filename_solved[] = "quizzes/arr_1_solved.txt";
   char filename_error[] = "quizzes/arr_1_with_error.txt";
 	char almost_solved[] = "quizzes/arr_1_almost_solved.txt";
 	char without_two[] = "quizzes/arr_1_without_two.txt";
-	char without_four[] = "quizzes/arr_1_without_four.txt";
+	
+	char sudoku_[] = "quizzes/arr_1_without_four.txt";
+	char sudoku_nr1_unsolved[] = "quizzes/arr_1_to_solve.txt";
+	char sudoku_nr2_unsolved[] = "quizzes/arr_6_unsolved.txt";
 
 	char silent[] = "-s";
 	bool showProgress = true;
 	
-	int *h_sudoku_solved, *h_sudoku_unsolved, *h_sudoku_error, *h_sudoku_almost_solved, *h_without_two, *h_without_four;
-	int a =5;
+	int *h_sudoku_to_solve, *h_sudoku_solved, *h_sudoku_unsolved, *h_sudoku_error, *h_sudoku_almost_solved, *h_without_two, *h_without_four;
+	int quizChoosing = argc;
 
 	if(argc > 1 && strcmp(argv[1], "-s") == 0)
 	{
 		printf("Włączony tryb cichy!\n");
+		quizChoosing -= 1;
 		showProgress = false;
 	}
+
+	if(quizChoosing == 0)
+		h_sudoku_to_solve = readSudokuArray(sudoku_nr1_unsolved);
+	else if(quizChoosing == 1)
+		h_sudoku_to_solve = readSudokuArray(sudoku_nr2_unsolved);
+
+
 	
 	//RETRIEVING SUDOKU QUIZ
 
 	h_sudoku_error = readSudokuArray(filename_error);
-	h_sudoku_unsolved = readSudokuArray(filename_unsolved); 
+	// h_sudoku_unsolved = readSudokuArray(filename_unsolved); 
   h_sudoku_solved = readSudokuArray(filename_solved);
 	h_sudoku_almost_solved = readSudokuArray(almost_solved);
 	h_without_two = readSudokuArray(without_two);
-	h_without_four = readSudokuArray(without_four);
+	// h_without_four = readSudokuArray(without_four);
 
 	//STARTING TIME MEASURMENT
 	clock_t begin = clock();
 	
 	//SOLVING SUDOKU 
-	cudaErrorHandling(solveSudoku(h_sudoku_solved, h_sudoku_unsolved, showProgress));
+	cudaErrorHandling(solveSudoku(h_sudoku_solved, h_sudoku_to_solve, showProgress));
 
 	//ENDING TIME MEASURMENT
 	clock_t end = clock();
